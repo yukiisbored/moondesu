91ce3662bc9e6057d7930f859e202c13869ae99f
Kaisar Arkhan <ykno@protonmail.com>
2017-02-01 10:12:27 +0700

    moondesu: Add command line argument to define config path
    Closes https://github.com/yukiisbored/moondesu/issues/10

--- moondesu.go
+++ moondesu.go
@@ -1,11 +1,25 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+)
+
+var configPath string
+
+func processArguments() {
+	flag.StringVar(&configPath,
+		"conf", "./config.toml", "Location of the config file")
+
+	flag.Parse()
+}
 
 func main() {
+	processArguments()
+
 	log.Println("starting moondesu")
 
-	_, err := loadConfiguration("./config.toml")
+	_, err := loadConfiguration(configPath)
 
 	if err != nil {
 		log.Panic(err)
